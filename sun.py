b9969e9ab1ec49bfa7b27b474a1c15b3f9b52645
valli28 <valli28@hotmail.com>
2020-03-03 16:20:34 +0100

    added comments with notes on how to do light sources in sun.py

--- sun.py
+++ sun.py
@@ -2,6 +2,14 @@ import numpy as np
 from pysolar.solar import *
 import datetime
 
+
+# Instead of calling this class "Sun" it could just be "light-source"
+# Since the sun is so far away from earth, it's basicly a laser. A common thing to do in games is just use it as a directional light
+# It's actually radial, like a bulb, but a reasonable approximation is to say that the sunrays are parallel at the surface of the earth.
+
+# The drone can be added as a light-source (since it emits thermal light) as well
+# The sun can be a light-source, and a boolean with "is_sun" or "is_directional" instead of radial check
+
 class Sun():
     def __init__(self, lat, lon):
         self.date_now = datetime.datetime.now(tz=pytz.UTC)
