11a73e9dd6d25551c2205ab285b143a5a9332557
Valthor B Gudmundsson <valli28@hotmail.com>
2020-03-10 17:17:46 +0100

    New approach instead of doing it with a normal vector to plane

--- boundary.py
+++ boundary.py
@@ -20,15 +20,18 @@ class Boundary():
         self.z = np.array([z0, z1, z2])
 
         # Vectors
-        p01 = self.p1 - self.p0
-        p02 = self.p2 - self.p0
-        self.cross = np.cross(p02, p01)
+        self.p01 = self.p1 - self.p0
+        self.p02 = self.p2 - self.p0
+        self.cross = np.cross(self.p02, self.p01)
         self.normal = self.cross / np.linalg.norm(self.cross)
 
-        distance = z2
 
         # Calculate plane
-        self.X, self.Y, self.Z = self.plane(np.linalg.norm(p01), np.linalg.norm(p02), 20, 20, self.normal, distance)
+        #self.X, self.Y, self.Z = self.plane(np.linalg.norm(self.p01), np.linalg.norm(self.p02), 20, 20, self.normal, self.z[2])
+
+
+        Nx = Ny = Nz = 10
+        self.X, self.Y, self.Z = self.plane_matrices(Nx, Ny, Nz)
 
         '''
         # a plane is a*x+b*y+c*z+d=0
@@ -49,61 +52,41 @@ class Boundary():
         self.reflectiveness = 1.0 # Default reflectiveness is 100% (mirror)
 
 
-    def rotmatrix(self, axis, costheta):
-        """ Calculate rotation matrix
-
-        Arguments:
-        - `axis`     : Rotation axis
-        - `costheta` : Rotation angle
-        """
-        x,y,z = axis
-        c = costheta 
-        s = sqrt(1-c*c)
-        C = 1-c
-
-        R = matrix([[ x*x*C+c,    x*y*C-z*s,  x*z*C+y*s ],
-                    [ y*x*C+z*s,  y*y*C+c,    y*z*C-x*s ],
-                    [ z*x*C-y*s,  z*y*C+x*s,  z*z*C+c]])
-
-        print(R)
-        angle = np.trace(R)
-
-        return R
-
-    def plane(self, Lx, Ly, Nx, Ny, n, d):
+    def plane_matrices(self, Nx, Ny, Nz):
         """ Calculate points of a generic plane 
-
         Arguments:
-        - `Lx` : Plane Length first direction
-        - `Ly` : Plane Length second direction
         - `Nx` : Number of points, first direction
         - `Ny` : Number of points, second direction
-        - `n`  : Plane orientation, normal vector
-        - `d`  : distance from the origin
+        - `Nz` : Number of points, third direction
+
+        >>> x = np.arange(-5, 5, 0.1)
+        >>> y = np.arange(-5, 5, 0.1)
+        >>> xx, yy = np.meshgrid(x, y, sparse=True)
+        >>> z = np.sin(xx**2 + yy**2) / (xx**2 + yy**2)
         """
 
-        x = linspace(-Lx/2,Lx/2,Nx)
-        y = linspace(-Ly/2,Ly/2,Ny)
-        # Create the mesh grid, of a XY plane sitting on the orgin
-        X,Y = meshgrid(x,y)
-        Z = zeros([Nx,Ny])
-        #n0 = array([0,0,1]) # This has to be something else. Maybe point in the same direction as the normal vector? This is stupid...
-        #n0 = n / np.linalg.norm(n)
-        #print(n0)
-        #n0 = self.p0 - self.p2
-        
-        # Rotate plane to the given normal vector
-        costheta = 0
-        axis     = self.p1 - self.p0 #array([1, 0, 0]) #self.normal
-        rotMatrix = self.rotmatrix(axis,costheta)
-        #print(rotMatrix)
-        XYZ = vstack([X.flatten(),Y.flatten(),Z.flatten()])
-        X,Y,Z = array(rotMatrix*XYZ).reshape(3,Nx,Ny)
+        # Based on the points and vectors, we draw planes that make sense. The matrices are not solved based on eachother or some plane formula due to divide-by-zero 
 
+        x = np.linspace(self.p0[0], self.p1[0], Nx)
+        y = np.linspace(self.p0[1], self.p1[1], Ny)
+        z = np.linspace(self.p0[2], self.p2[2], Nz)
+
+
+        print("xyz's")
+        print(x)
+        print(y)
+        print(z)
         
-        dVec = (n/norm(n))*d # Distance vector from origin parallel to normal-vector of plane
-        X,Y,Z = X+dVec[0],Y+dVec[1],Z+dVec[2]
-        return X,Y,Z
+        xx, yy = np.meshgrid(x, y)
+        zz,  = np.meshgrid(z)
+
+        print("xxyyzz's")
+        print(xx)
+        print(yy)
+        print(zz)
+        
+    
+        return xx, yy, zz
 
 
     def plot(self, axes):
