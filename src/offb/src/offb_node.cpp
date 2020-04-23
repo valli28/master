26eaad130dfd27402d1785d7635a8124328c5916
valli28 <valli28@hotmail.com>
2020-04-23 10:55:58 +0200

    Commiting deletion of Firmware

--- src/offb/src/offb_node.cpp
+++ src/offb/src/offb_node.cpp
@@ -20,27 +20,33 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
 
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+            ("mavros/state", 10, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+            ("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+            ("mavros/set_mode");
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
+
+    
 
     // wait for FCU connection
     while(ros::ok() && !current_state.connected){
         ros::spinOnce();
         rate.sleep();
     }
+    
 
     geometry_msgs::PoseStamped pose;
     pose.pose.position.x = 0;
     pose.pose.position.y = 0;
     pose.pose.position.z = 2;
 
-
-    //send a few setpoints before starting because apparently PX4 will deny Offboard mode if it hasnt started the setpoint stream.... 
+    //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
         local_pos_pub.publish(pose);
         ros::spinOnce();
@@ -54,36 +60,27 @@ int main(int argc, char **argv)
     arm_cmd.request.value = true;
 
     ros::Time last_request = ros::Time::now();
-    bool begin_flying_forwards = false;
 
-    ROS_INFO("gafdsdfasdfasdf");
-    
     while(ros::ok()){
-        ROS_INFO("It's running");
-        if( current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(5.0)))
-        {
-            if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent)
-            {
+        if( current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent){
                 ROS_INFO("Offboard enabled");
             }
             last_request = ros::Time::now();
         } else {
-            if( !current_state.armed && (ros::Time::now() - last_request > ros::Duration(5.0)))
-            {
-                if( arming_client.call(arm_cmd) && arm_cmd.response.success)
-                {
+            if( !current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
                     ROS_INFO("Vehicle armed");
-                    begin_flying_forwards = true;
                 }
                 last_request = ros::Time::now();
             }
         }
 
         local_pos_pub.publish(pose);
-
-        if (begin_flying_forwards == true){
-            pose.pose.position.x = pose.pose.position.x + 0.05; // Fly in the x-direction with 1 meters per second.
-        }
 
         ros::spinOnce();
         rate.sleep();
