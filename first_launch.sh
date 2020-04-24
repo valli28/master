266f885c8a3afbf84323852ebac4c17682224ce4
valli28 <valli28@hotmail.com>
2020-04-24 17:53:48 +0200

    Now works as a ros system in simulation with Gazebo and PX4. Use launch-common.sh to launch system.

--- first_launch.sh
+++ first_launch.sh
@@ -7,4 +7,7 @@ source $px4_dir/Tools/setup_gazebo.bash $px4_dir $px4_dir/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$px4_dir
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$px4_dir/Tools/sitl_gazebo
 
-roslaunch offb movement.launch
+roslaunch px4 posix_sitl.launch
+
+
+
